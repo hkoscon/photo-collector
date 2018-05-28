cb0972d78fcece8b41e5ed5e03386f045373d21c
Tony Yip <tonyhhyip@gmail.com>
2018-05-29 01:15:59 +0800

    Fix the test

--- pkg/crypto/decrypt_test.go
+++ pkg/crypto/decrypt_test.go
@@ -1,8 +1,6 @@
 package crypto
 
 import (
-	"crypto/x509"
-	"io/ioutil"
 	"testing"
 )
 
@@ -11,19 +9,8 @@ func TestCrypt_loadKey(t *testing.T) {
 		PriKeyPath: "pri_test.pem",
 	}
 
-	crypt.loadKey([]byte("1234"))
+	crypt.LoadKey([]byte("1234"))
 	if crypt.privateKey == nil {
 		t.Fatal("fail to load private key")
-	}
-
-	keyData, _ := ioutil.ReadFile("pub_test.pem")
-
-	pubKey, err := x509.ParsePKCS1PublicKey(keyData)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if pubKey.E != crypt.privateKey.PublicKey.E {
-		t.Fatal("different E")
 	}
 }
